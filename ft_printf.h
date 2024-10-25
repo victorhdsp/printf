f6d497faf53add339d91baaa97ff42d9ebe4493c
vide-sou <victorhugods.pereira@gmail.com>
2024-10-25 11:02:59 -0300

    finish

--- ft_printf.h
+++ ft_printf.h
@@ -1,37 +1,50 @@
-# ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 11:02:17 by vide-sou          #+#    #+#             */
+/*   Updated: 2024/10/25 11:02:18 by vide-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdarg.h>
-#include "libft.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-#define HEX "0123456789abcdef"
-#define DEC "0123456789"
+# include "libft.h"
+# include <stdarg.h>
+# include <unistd.h>
 
-typedef struct  s_flag_data
+# define HEX "0123456789abcdef"
+# define DEC "0123456789"
+
+typedef struct s_flag_data
 {
-    size_t  width;
-    int     has_precision;
-    size_t  precision;
-    int     align;
-    char    set;
-    char    *prefix;
-}               t_flag_data;
+	size_t	width;
+	int		has_precision;
+	size_t	precision;
+	int		align;
+	char	set;
+	char	*prefix;
+}			t_flag_data;
 
-int     ft_printf(const char *str, ...);
-int     ft_putchar(char c);
-int     ft_putstr(char *str);
-char	*ft_convert_base(unsigned long long nb, const char *base);
+int			ft_printf(const char *str, ...);
+int			ft_putchar(char c);
+int			ft_putstr(char *str);
+char		*ft_convert_base(unsigned long long nb, const char *base);
 t_flag_data	*ft_create_flag_data(void);
-void	ft_get_flag_data(char *flag, t_flag_data *flag_data);
-char	*ft_fill(t_flag_data *flag_data, char *str);
-int     ft_command_c_n(char *flag, int c);
-int     ft_command_s_n(char *flag, char *str);
-int     ft_command_d_n(char *flag, long nb);
-int ft_command_hex_n(t_flag_data *flag_data, unsigned long nb, int upper);
-int     ft_command_x_n(char *flag, unsigned int nb);
-int     ft_command_upperx_n(char *flag, unsigned int nb);
-int     ft_command_p_n(char *flag, void *point);
-char	*ft_substr(t_flag_data *flag_data, char *str);
+void		ft_get_flag_data(char *flag, t_flag_data *flag_data);
+char		*ft_fill(t_flag_data *flag_data, char *str);
+int			ft_command_c_n(char *flag, int c);
+int			ft_command_s_n(char *flag, char *str);
+int			ft_command_d_n(char *flag, long nb);
+int			ft_command_hex_n(t_flag_data *flag_data, unsigned long nb,
+				int upper);
+int			ft_command_x_n(char *flag, unsigned int nb);
+int			ft_command_upperx_n(char *flag, unsigned int nb);
+int			ft_command_p_n(char *flag, void *point);
+char		*ft_substr(t_flag_data *flag_data, char *str);
 
 #endif
--- libft.h
+++ libft.h
@@ -1,8 +1,20 @@
-# ifndef LIBFT_H
-#define LIBFT_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 11:02:20 by vide-sou          #+#    #+#             */
+/*   Updated: 2024/10/25 11:02:21 by vide-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "ft_printf.h"
 # include <stdlib.h>
-#include "ft_printf.h"
 
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memset(void *str, int c, size_t n);
@@ -11,6 +23,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
-int     ft_toupper(int c);
+int		ft_toupper(int c);
 
 #endif
